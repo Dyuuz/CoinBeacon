882d37783d2e3113680f7b3e4d976a7e195cdbd5
Dyuuz <lekanoyesunle@gmail.com>
2025-07-28 15:26:30 +0100

    feat:update dynamic Snappy rate time

--- JavaScript/Refresh.js
+++ JavaScript/Refresh.js
@@ -4,10 +4,12 @@ try {
     const btcValue = response.data[0].btc;
     const ethValue = response.data[0].eth;
     const usdtValue = response.data[0].usdt;
+    const time_updated = response.data[0].updated_time;
 
-    document.querySelector('.snappy-btc-sell').textContent = `₦${btcValue || '--'}`;
-    document.querySelector('.snappy-eth-sell').textContent = `₦${ethValue || '--'}`;
-    document.querySelector('.snappy-usdt-sell').textContent = `₦${usdtValue || '--'}`;
+    document.querySelector('.snappy-btc-sell').textContent = `₦${btcValue || 'pending'}`;
+    document.querySelector('.snappy-eth-sell').textContent = `₦${ethValue || 'pending'}`;
+    document.querySelector('.snappy-usdt-sell').textContent = `₦${usdtValue || 'pending'}`;
+    document.querySelector('.snappy-time').textContent = `${time_updated || 'updating'}`;
 } catch (error) {
     document.querySelector('.snappy-btc-sell').textContent = "pending";
     document.querySelector('.snappy-eth-sell').textContent = "pending";
