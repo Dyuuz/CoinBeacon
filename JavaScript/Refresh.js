9691d381c08bdce863f0458fde32d185d196930b
Dyuuz <lekanoyesunle@gmail.com>
2025-07-28 15:20:26 +0100

    feat:update dynamic Snappy Rateall assets

--- JavaScript/Refresh.js
+++ JavaScript/Refresh.js
@@ -1,19 +1,21 @@
 async function updateRate() {
 try {
     const response = await axios.get('https://fantastic-rejoicing-prod.up.railway.app/rates/snappy-exchange');
-    const btcValue = data[0].btc;
-    const ethValue = data[0].eth;
-    const usdtValue = data[0].usdt;
+    const btcValue = response.data[0].btc;
+    const ethValue = response.data[0].eth;
+    const usdtValue = response.data[0].usdt;
 
-    // Update the BTC rate in the DOM
     document.querySelector('.snappy-btc-sell').textContent = `₦${btcValue || '--'}`;
+    document.querySelector('.snappy-eth-sell').textContent = `₦${ethValue || '--'}`;
+    document.querySelector('.snappy-usdt-sell').textContent = `₦${usdtValue || '--'}`;
 } catch (error) {
+    document.querySelector('.snappy-btc-sell').textContent = "pending";
+    document.querySelector('.snappy-eth-sell').textContent = "pending";
+    document.querySelector('.snappy-usdt-sell').textContent = "pending";
     console.error('Failed to fetch BTC rate:', error);
 }
 }
 
-// Call once on page load
 updateRate();
 
-// Set interval to call it every 60 seconds (60000 ms)
 setInterval(updateRate, 60000);
